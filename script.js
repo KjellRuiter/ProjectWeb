5ef35cf00f2c45baa1fbf765701af26e3fd5851d
Kjell Ruiter <kjellruiter@hotmail.com>
2018-01-15 17:49:09 +0100

    helemaal mooi
    dasdfasdf

---
+++ script.js
@@ -0,0 +1,61 @@
+var add_button = document.querySelector(".add");
+var download_button = document.querySelector(".download");
+var readbar = document.querySelector(".readbar span");
+var readbar_plane = document.querySelector(".readbar span i");
+var filter_button = document.querySelector(".filter_button");
+
+var scrolled = 0;
+var total_height = 0;
+
+if (add_button) {
+   add_button.addEventListener('click', showDownload); 
+}
+
+if (download_button) {
+  download_button.addEventListener ('click',veranderDownload);  
+}
+
+
+function showDownload () {
+    add_button.classList.toggle("active");
+    download_button.classList.toggle("active");
+}
+
+function veranderDownload () {
+    download_button.classList.toggle("process");
+}
+
+if (readbar) {
+    window.addEventListener("scroll", function(e){
+        // determine direction
+        var direcction_down = scrolled < window.scrollY;
+        
+        scrolled = window.scrollY;
+        total_height = document.body.scrollHeight;
+
+        // 20% offset
+        var percentage_scrolled = 20 + Math.round(scrolled / total_height * 100);
+        readbar.style.width = percentage_scrolled + "%";
+        
+        if (direcction_down === false) {
+            readbar_plane.classList.add("direction_up");
+        } else {
+            readbar_plane.classList.remove("direction_up");
+        }
+        
+        if (percentage_scrolled >= 100 ) {
+            readbar.style.backgroundColor = "#00D66C"; 
+        } else {
+            readbar.style.backgroundColor = "#ff3268"; 
+        }
+    })   
+    
+}
+
+if (filter_button) {
+    filter_button.addEventListener ('click', showFilter);
+}
+
+function showFilter() {
+    document.querySelector(".filters").classList.toggle("active");
+}
